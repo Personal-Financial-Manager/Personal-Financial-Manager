fddd216a93e084548981aeecbbf9fde8c1677b32
NedaJavan <nedajavan747@gmail.com>
2023-06-04 16:08:03 +0330

    Refactor MainActivity package.

--- app/src/main/java/com/example/personal_financial_manager/ui/MainActivity.kt
+++ app/src/main/java/com/example/personal_financial_manager/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.personal_financial_manager
+package com.example.personal_financial_manager.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.personal_financial_manager.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
