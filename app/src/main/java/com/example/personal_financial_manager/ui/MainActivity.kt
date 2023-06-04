ee8d9776593867fe3d8cb5d63aa36c709ac9e646
AliMohebiFar <ssmario2013@gmail.com>
2023-06-04 23:32:14 +0330

    [Add] navigation to MainActivity

--- app/src/main/java/com/example/personal_financial_manager/ui/MainActivity.kt
+++ app/src/main/java/com/example/personal_financial_manager/ui/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.personal_financial_manager.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.personal_financial_manager.R
+import com.example.personal_financial_manager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initNavController()
+    }
+
+    private fun initNavController() {
+        navController = Navigation.findNavController(this, R.id.host_fragment_main)
     }
 }
