fe0fb63e4bc4497a1ea331ca2f703e796eb28a00
AliMohebiFar <ssmario2013@gmail.com>
2023-06-04 22:34:21 +0330

    [Add] BudgetListFragment classes.

---
+++ app/src/main/java/com/example/personal_financial_manager/ui/home/budget/BudgetListFragment.kt
@@ -0,0 +1,29 @@
+package com.example.personal_financial_manager.ui.home.budget
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.personal_financial_manager.R
+import com.example.personal_financial_manager.databinding.FragmentBudgetListBinding
+
+class BudgetListFragment : Fragment() {
+
+    private var _binding: FragmentBudgetListBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = BudgetListFragment()
+    }
+
+    private val viewModel: BudgetListViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_budget_list, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/personal_financial_manager/ui/home/budget/BudgetListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.personal_financial_manager.ui.home.budget
+
+import androidx.lifecycle.ViewModel
+
+class BudgetListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
