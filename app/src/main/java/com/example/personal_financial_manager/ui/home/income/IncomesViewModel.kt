f64a291a44a8e63fdcfc67d5140be9b7c5b2280c
AliMohebiFar <ssmario2013@gmail.com>
2023-06-04 22:35:05 +0330

    [Add] Incomes classes.

---
+++ app/src/main/java/com/example/personal_financial_manager/ui/home/income/IncomesViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.personal_financial_manager.ui.home.income
+
+import androidx.lifecycle.ViewModel
+
+class IncomesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
