0f9f5db4c5258c0737fba47b89e85423f381a389
NedaJavan <nedajavan747@gmail.com>
2023-06-04 14:20:35 +0330

    Add income and budgeting tables.

---
+++ app/src/main/java/com/example/personal_financial_manager/data/local/budgeting/Budgeting.kt
@@ -0,0 +1,15 @@
+package com.example.personal_financial_manager.data.local.budgeting
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Budgeting(
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "Title") val title: String,
+    @ColumnInfo(name = "Price") val price: Long,
+    @ColumnInfo(name = "Date") val date: Long,
+    @ColumnInfo(name = "Description") val description: String,
+    @ColumnInfo(name = "BudgetingCategory") val budgetingCategory: String,
+)
