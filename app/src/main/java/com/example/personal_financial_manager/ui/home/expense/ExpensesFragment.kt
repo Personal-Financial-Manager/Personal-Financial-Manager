fe88d0fef3ec6a38fd29e8d492b69319cc8ba5cc
AliMohebiFar <ssmario2013@gmail.com>
2023-06-04 22:34:46 +0330

    [Add] Expenses classes.

---
+++ app/src/main/java/com/example/personal_financial_manager/ui/home/expense/ExpensesFragment.kt
@@ -0,0 +1,29 @@
+package com.example.personal_financial_manager.ui.home.expense
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.personal_financial_manager.R
+import com.example.personal_financial_manager.databinding.FragmentExpensesBinding
+
+class ExpensesFragment : Fragment() {
+
+    private var _binding: FragmentExpensesBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = ExpensesFragment()
+    }
+
+    private val viewModel: ExpensesViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_expenses, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/personal_financial_manager/ui/home/expense/ExpensesViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.personal_financial_manager.ui.home.expense
+
+import androidx.lifecycle.ViewModel
+
+class ExpensesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
