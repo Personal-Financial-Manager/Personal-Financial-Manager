0977d717379aac7072b635d948dc6e6fdc21f49d
AliMohebiFar <ssmario2013@gmail.com>
2023-07-17 21:48:49 +0330

    Add the new expense button.

--- app/src/main/java/com/example/personal_financial_manager/ui/expense/ExpensesFragment.kt
+++ app/src/main/java/com/example/personal_financial_manager/ui/expense/ExpensesFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.personal_financial_manager.R
 import com.example.personal_financial_manager.databinding.FragmentExpensesBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ExpensesFragment : Fragment() {
 
@@ -25,6 +27,21 @@ class ExpensesFragment : Fragment() {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_expenses, container, false)
         binding.lifecycleOwner = this
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        initOnClickListeners()
+    }
+
+    private fun initOnClickListeners() {
+        binding.fabExpenses.setOnClickListener {
+            Toast.makeText(requireContext(), "Test", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
