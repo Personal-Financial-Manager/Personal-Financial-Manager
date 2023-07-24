70f546cc9f9b4d990f6bc7f6ecebb6daa352774d
AliMohebiFar <ssmario2013@gmail.com>
2023-07-24 22:28:20 +0330

    Add ExpensesRepositoryTestDouble.

--- app/src/test/java/com/example/personal_financial_manager/expenses/ExpensesUseCaseTest.kt
+++ app/src/test/java/com/example/personal_financial_manager/expenses/ExpensesUseCaseTest.kt
@@ -1,20 +1,21 @@
 package com.example.personal_financial_manager.expenses
 
 import com.example.personal_financial_manager.data.ExpensesRepository
-import com.example.personal_financial_manager.data.ExpensesRepositoryImp
+import com.example.personal_financial_manager.data.local.entity.Expense
 import com.example.personal_financial_manager.usecase.expenses.ExpensesUseCase
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
+import javax.inject.Inject
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class ExpensesUseCaseTest {
+class ExpensesUseCaseTest @Inject constructor() {
 
     private lateinit var testSubject: ExpensesUseCase
     private lateinit var repository: ExpensesRepository
 
     @BeforeAll
     fun init() {
-        repository = ExpensesRepositoryImp()
+        repository = ExpensesRepositoryTestDouble()
         testSubject = ExpensesUseCase(repository)
     }
 
@@ -22,5 +23,11 @@ class ExpensesUseCaseTest {
     fun fetchExpenses_emptyInput_returnsListOfExpenses() {
         val expensesList = testSubject.fetchExpenses()
         assertEquals(0, expensesList.size)
+    }
+}
+
+class ExpensesRepositoryTestDouble() : ExpensesRepository {
+    override fun fetchExpenses(): List<Expense> {
+        return listOf()
     }
 }
