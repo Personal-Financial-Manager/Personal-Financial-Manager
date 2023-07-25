ce0733f62c556f381fcb6754404de74f2bee929b
AliMohebiFar <ssmario2013@gmail.com>
2023-07-25 22:52:35 +0330

    Add ExpensesRepositoryImp and it's injection.

--- app/src/main/java/com/example/personal_financial_manager/data/ExpensesRepositoryImp.kt
+++ app/src/main/java/com/example/personal_financial_manager/data/ExpensesRepositoryImp.kt
@@ -1,11 +1,12 @@
 package com.example.personal_financial_manager.data
 
+import com.example.personal_financial_manager.data.local.db.FinancialDatabase
 import com.example.personal_financial_manager.data.local.entity.Expense
 import javax.inject.Inject
 
-class ExpensesRepositoryImp @Inject constructor(): ExpensesRepository {
+class ExpensesRepositoryImp @Inject constructor(val database: FinancialDatabase) : ExpensesRepository {
 
-    override fun fetchExpenses(): List<Expense> {
-        return listOf()
+    override suspend fun fetchExpenses(): List<Expense> {
+        return database.expenseDao().getAllExpenses()
     }
 }
--- app/src/main/java/com/example/personal_financial_manager/di/ExpensesModule.kt
+++ app/src/main/java/com/example/personal_financial_manager/di/ExpensesModule.kt
@@ -2,17 +2,20 @@ package com.example.personal_financial_manager.di
 
 import com.example.personal_financial_manager.data.ExpensesRepository
 import com.example.personal_financial_manager.data.ExpensesRepositoryImp
+import com.example.personal_financial_manager.data.local.db.FinancialDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class ExpensesModule {
 
     @Provides
-    fun provideExpensesRepository(): ExpensesRepository {
-        return ExpensesRepositoryImp()
+    @Singleton
+    fun provideExpensesRepository(database: FinancialDatabase): ExpensesRepository {
+        return ExpensesRepositoryImp(database)
     }
 }
