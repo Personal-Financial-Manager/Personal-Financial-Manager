1098850a0aa35d12f307faa3e13d4015d1d32deb
AliMohebiFar <ssmario2013@gmail.com>
2023-07-25 22:53:53 +0330

    Refactor ExpensesUseCaseTest

--- app/src/test/java/com/example/personal_financial_manager/expenses/ExpensesUseCaseTest.kt
+++ app/src/test/java/com/example/personal_financial_manager/expenses/ExpensesUseCaseTest.kt
@@ -3,6 +3,9 @@ package com.example.personal_financial_manager.expenses
 import com.example.personal_financial_manager.data.ExpensesRepository
 import com.example.personal_financial_manager.data.local.entity.Expense
 import com.example.personal_financial_manager.usecase.expenses.ExpensesUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import javax.inject.Inject
@@ -21,13 +24,15 @@ class ExpensesUseCaseTest @Inject constructor() {
 
     @Test
     fun fetchExpenses_emptyInput_returnsListOfExpenses() {
-        val expensesList = testSubject.fetchExpenses()
-        assertEquals(0, expensesList.size)
+        CoroutineScope(Dispatchers.IO).launch {
+            val expensesList = testSubject.fetchExpenses()
+            assertEquals(0, expensesList.size)
+        }
     }
 }
 
 class ExpensesRepositoryTestDouble() : ExpensesRepository {
-    override fun fetchExpenses(): List<Expense> {
+    override suspend fun fetchExpenses(): List<Expense> {
         return listOf()
     }
 }
